404172cebfbeba2fcb008b4000a707500f69fcae
Thiago Luis <thiagoluisrpinho@gmail.com>
2019-06-17 14:47:56 -0300

    Add: HOST and PORT constants, some first lines using sockets for udp

--- server.py
+++ server.py
@@ -1,6 +1,13 @@
-# Código desenvolvimento para a matéria de Teleinformática e Redes I de 2019.1 
+# Código desenvolvido para a matéria de Teleinformática e Redes I de 2019.1
+# da Universidade de Brasília
 # O Objetivo do projeto é criar um chat multi-servidores com diversos usuários
 
-
 from socket import AF_INET, socket, SOCK_STREAM
-from threading import Thread
+# É uma biblioteca python voltada para a interface de soquetes(TCP/UDP).
+
+HOST = '127.0.0.1' # ENDEREÇO IP DO HOST
+PORT = 3300        # PORTA DO SERVIDOR(SERVIDOR ESCUTA)
+
+udp = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+orig = (HOST, PORT)
+udp.bind(orig)     # Bind the socket to address.
