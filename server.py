5f907921966aca5df3ee4d89782849f1f000d2df
Thiago Luis <thiagoluisrpinho@gmail.com>
2019-06-17 14:35:53 -0300

    Add: server.py and some initials imports

---
+++ server.py
@@ -0,0 +1,6 @@
+# Código desenvolvimento para a matéria de Teleinformática e Redes I de 2019.1 
+# O Objetivo do projeto é criar um chat multi-servidores com diversos usuários
+
+
+from socket import AF_INET, socket, SOCK_STREAM
+from threading import Thread
