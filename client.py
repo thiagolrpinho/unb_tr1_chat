3add9392cbb93007cecdd429f606c99e0c185a7d
Thiago Luis <thiagoluisrpinho@gmail.com>
2019-06-26 18:45:54 -0300

    Refactor: Deletei funcoes de exemplo de cliente.py

--- client.py
+++ client.py
@@ -11,27 +11,6 @@ HOST = LOCAL_IP    # ENDEREÇO IP SERVIDOR
 PORT = 3300        # PORTA DO SERVIDOR(CLIENTE ENVIA)
 BUFF_SIZE = 1024
 
-def start_cliente_udp():
-  socket_udp = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-  endereco_de_destino = (HOST, PORT)
-  print("Para sair envie uma mensagem com um espaço em branco")
-  mensagem_a_enviar = input()
-  while mensagem_a_enviar != ' ':
-    socket_udp.sendto(bytes(mensagem_a_enviar, 'utf8'), endereco_de_destino)
-    mensagem_a_enviar = input()
-  socket_udp.close
-
-def start_cliente_tcp():
-  socket_tcp = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-  endereco_de_destino = (HOST, PORT)
-  socket_tcp.connect(endereco_de_destino)
-  print("Para sair envie uma mensagem com um espaço em branco")
-  mensagem_a_enviar = input()
-  while mensagem_a_enviar != ' ':
-    socket_tcp.send(bytes(mensagem_a_enviar, 'utf8'))
-    mensagem_a_enviar = input()
-  socket_tcp.close()
-
 def receive_message(conexao_chat_server):
   ligado = True
   while ligado:
@@ -56,22 +35,6 @@ def send_message(mensagem_a_enviar, conexoes_chat_servers, evento = None):
         continue
       except OSError:
         break
-
-def start_chat_user():
-  conexao_chat_server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-  # Soquete TCP
-  endereco_de_destino = (HOST, PORT)
-  # Tenta criar uma conexão com o servidor de destino
-  conexao_chat_server.connect(endereco_de_destino)
-  receive_thread = Thread(target=receive_message, args=(conexao_chat_server,))
-  receive_thread.start()
-  mensagem_a_enviar = input()
-  while mensagem_a_enviar != '{quit}':
-    send_message(mensagem_a_enviar, conexao_chat_server)
-    mensagem_a_enviar = input()
-  # Manda uma informando para o server que irá fechar a conexão
-  send_message(mensagem_a_enviar, conexao_chat_server)
-  conexao_chat_server.close()
 
 def start_chat_user_with_multi_servers():
   conexoes_chat_servers = []
