fd1e14a11e1c9f501b57225c81ee6da26ac3cba6
Vinícius Gomes <vgdsouza@live.com>
2019-06-27 10:45:48 -0300

    multiservers pronto

--- client.py
+++ client.py
@@ -8,22 +8,63 @@ import socket
 import tkinter as tk
 from threading import Thread
 
+MAX_SERVERS = 50
 LOCAL_IP = '127.0.0.1'
 HOST = LOCAL_IP    # ENDEREÇO IP SERVIDOR
 PORT = 3300        # PORTA DO SERVIDOR(CLIENTE ENVIA)
 BUFF_SIZE = 1024
-CONEXAO = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 DESTINO = (HOST, PORT)
+
 ROOT = tk.Tk()
 CURRENT_FRAME = None
 NOME = None
 SALA = None
 MENSAGEM = None
 CHAT = None
+
+CONEXAO = None
 EXIT = False
 
+def connect_to_server():
+  global CONEXAO, EXIT
+
+  try:
+    CONEXAO.close()
+  except:
+    pass
+  finally:
+    CONEXAO = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    EXIT = True
+    for i in range(MAX_SERVERS):
+      DESTINO = (HOST, PORT + i)
+      try:
+        CONEXAO.connect(DESTINO)
+      except:
+        pass
+      else:
+        EXIT = False
+        break
+
+  if not EXIT:
+    CONEXAO.send(bytes(SALA.get(), "utf8"))
+    time.sleep(.1)
+    mensagem = CONEXAO.recv(BUFF_SIZE).decode("utf8")
+    if mensagem != '':
+      print(mensagem)
+      CHAT.config(state='normal')
+      CHAT.insert('end', mensagem + '\n')
+      CHAT.see('end')
+      CHAT.config(state='disabled')
+    CONEXAO.send(bytes(NOME.get(), "utf8"))
+    time.sleep(.1)
+
+  CHAT.config(state='normal')
+  CHAT.insert('end', 'Servidor diz: Conectado!\n')
+  CHAT.see('end')
+  CHAT.config(state='disabled')
+
 def chat_gui():
-  global CURRENT_FRAME, MENSAGEM, CHAT
+  global CURRENT_FRAME, MENSAGEM, CHAT, PORT, DESTINO, EXIT
   ROOT.title(NOME.get() + ' em: Sala ' + SALA.get())
 
   chatFrame = tk.Frame(ROOT, padx=10, pady=10)
@@ -46,15 +87,10 @@ def chat_gui():
 
   CURRENT_FRAME = chatFrame
 
-  CONEXAO.connect(DESTINO)
+  connect_to_server()
 
   receive_thread = Thread(target=receive_message, args=())
   receive_thread.start()
-
-  CONEXAO.send(bytes(SALA.get(), "utf8"))
-  time.sleep(.1)
-  CONEXAO.send(bytes(NOME.get(), "utf8"))
-  time.sleep(.1)
 
 def start_gui():
   global CURRENT_FRAME, NOME, SALA
@@ -85,33 +121,51 @@ def start_gui():
   CURRENT_FRAME = loginFrame
 
 def receive_message():
-  global CHAT
+  global CHAT, EXIT, DESTINO
 
   while not EXIT:
-    try: mensagem = CONEXAO.recv(BUFF_SIZE).decode("utf8")
-    except OSError: break
-    CHAT.config(state='normal')
-    CHAT.insert('end', mensagem + '\n')
-    CHAT.see('end')
-    CHAT.config(state='disabled')
+    mensagem = CONEXAO.recv(BUFF_SIZE).decode("utf8")
+    if mensagem != '':
+      print(mensagem)
+      CHAT.config(state='normal')
+      CHAT.insert('end', mensagem + '\n')
+      CHAT.see('end')
+      CHAT.config(state='disabled')
+    else:
+      connect_to_server()
+
+  CHAT.config(state='normal')
+  CHAT.insert('end', 'Servidor diz: Desconectado!\n')
+  CHAT.see('end')
+  CHAT.config(state='disabled')
 
 def send_message(event = None):
   if MENSAGEM.get() == '{quit}':
     quit_chat()
   else:
-    CONEXAO.send(bytes(MENSAGEM.get(), "utf8"))
-    MENSAGEM.delete(0, 'end')
+    try:
+      CONEXAO.send(bytes(MENSAGEM.get(), "utf8"))
+    except:
+      if EXIT:
+        ROOT.quit()
+    else:
+      MENSAGEM.delete(0, 'end')
 
 def quit_chat():
   global EXIT
-  CONEXAO.send(bytes("{quit}", "utf8"))
-  CONEXAO.close()
+
+  try:
+    CONEXAO.send(bytes("{quit}", "utf8"))
+    CONEXAO.close()
+  except:
+    pass
+
   EXIT = True
   ROOT.quit()
 
-def start_chat_user():
+def main():
   start_gui()
   ROOT.mainloop()
 
 if __name__ == '__main__':
-  start_chat_user()
+  main()
--- server.py
+++ server.py
@@ -5,30 +5,30 @@
 # É uma biblioteca python voltada para a interface de soquetes(TCP/UDP).
 import socket
 from threading import Thread
+import time
 
 LOCAL_IP = '127.0.0.1'
-HOST = LOCAL_IP  # ENDEREÇO IP DO HOST
-PORT = 3300        # PORTA DO SERVIDOR(SERVIDOR ESCUTA)
+HOST = LOCAL_IP   # ENDEREÇO IP DO HOST
+PORT = 3300       # PORTA DO SERVIDOR(SERVIDOR ESCUTA)
 BUFF_SIZE = 1024
 MAX_USERS = 5
 
 def conexao_usuario(chat_server, usuarios, usuario):
   room = int(usuario.recv(BUFF_SIZE).decode("utf8"))
-  welcome = 'Bem-Vindo!'
+  welcome = 'Servidor diz: Bem-Vindo!'
   usuario.send(bytes(welcome, "utf8"))
   nickname = usuario.recv(BUFF_SIZE).decode("utf8")
-  mensagem = "%s se juntou ao chat" % nickname
+  mensagem = "%s se juntou ao chat!" % nickname
   broadcast(usuarios[room], bytes(mensagem, "utf8"))
   usuarios[room][usuario] = nickname
   bytes_recebidos = usuario.recv(BUFF_SIZE)
   while str(bytes_recebidos, encoding='utf8') != '{quit}':
-    # print(str(bytes_recebidos, encoding='utf8'))
-    broadcast(usuarios[room], bytes_recebidos, usuarios[room][usuario])
+    if str(bytes_recebidos, encoding='utf8') != '':
+      broadcast(usuarios[room], bytes_recebidos, usuarios[room][usuario])
     bytes_recebidos = usuario.recv(BUFF_SIZE) # Retorna o buffer e o endereço IP de origem
-
-  usuario.close()
-  del usuarios[room][usuario]
   broadcast(usuarios[room], bytes("%s está sem tempo, irmão." % nickname, "utf8"))
+  del usuarios[room][usuario]
+  usuario.close()
 
 def recebe_usuario(chat_server, usuarios):
   enderecos = dict()
@@ -41,28 +41,35 @@ def recebe_usuario(chat_server, usuarios):
   except KeyboardInterrupt:
     pass
 
-def broadcast(usuarios, mensagem_a_transmitir, autor = "Servidor "):
+def broadcast(usuarios, mensagem_a_transmitir, autor = "Servidor"):
   if usuarios:
     for socket in usuarios:
       socket.send(bytes(autor  + " diz: ","utf8") + mensagem_a_transmitir )
 
 def start_chat_server():
+  global PORT
+
   try:
     chat_server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     endereco_para_escutar = (HOST, PORT)
-    chat_server.bind(endereco_para_escutar)     # Esse socket estará ligado a esse endereço
-    print("Servidor online: Escutando mensagens")
+    while True:
+      try: # Esse socket estará ligado a esse endereço
+        chat_server.bind(endereco_para_escutar)
+      except OSError: # Caso o socket esteja ocupado
+        PORT = PORT + 1
+        endereco_para_escutar = (HOST, PORT)
+        continue
+      else:
+        break
+    print("Servidor (" + str(PORT) + ") online: Escutando mensagens")
     chat_server.listen(MAX_USERS)  ## escuta no máximo 5 conexões
     usuarios = [dict() for i in range(MAX_USERS)]
 
-    ACCEPT_THREAD = Thread(target=recebe_usuario, args=(chat_server, usuarios,))
+    ACCEPT_THREAD = Thread(target=recebe_usuario, args=(chat_server, usuarios))
     ACCEPT_THREAD.start()
     ACCEPT_THREAD.join()
   except KeyboardInterrupt:
     chat_server.close()
-    pass
 
 
 start_chat_server()
-#start_server_tcp()
-#start_server_udp()
